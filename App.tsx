d532eaea843647f2770bcb963552391c8aca8166
m-annasengineer <m.annas.engineer@gmail.com>
2026-04-15 00:53:10 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -552,9 +552,9 @@ const Footer = () => {
         </div>
         
         <div className="flex items-center gap-8">
-          <a href="#" className="text-text-muted hover:text-teal text-sm transition-colors">LinkedIn</a>
-          <a href="#" className="text-text-muted hover:text-teal text-sm transition-colors">GitHub</a>
-          <a href="#" className="text-text-muted hover:text-teal text-sm transition-colors">Email</a>
+          <a href="https://www.linkedin.com/in/muhammad-annas-38422b403/" target = "_blank" className="text-text-muted hover:text-teal text-sm transition-colors">LinkedIn</a>
+          <a href="https://github.com/m-annasengineer" target = "_blank" className="text-text-muted hover:text-teal text-sm transition-colors">GitHub</a>
+          <a href="m.annas.engineer@gmail.com" className="text-text-muted hover:text-teal text-sm transition-colors">Email</a>
         </div>
       </div>
     </footer>
